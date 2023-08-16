f220962afef948da342c47e571865372c7206688
hyi <hongyi@renci.org>
2023-08-16 16:05:05 -0400

    removed all rows with target column being null

--- process_data.py
+++ process_data.py
@@ -17,10 +17,11 @@ if __name__ == '__main__':
     input_df = input_df.dropna(axis=1, how='all')
     print(f'after removing all null columns: {input_df.shape}')
     pd.set_option('display.max_rows', None)
+    basename = os.path.splitext(output_data)[0]
+    input_df_null = input_df[input_df['Y/N/P'].isna()]
+    input_df_null.to_csv(f'{basename}_null.csv', index=False)
+    # remove rows with target column Y/N/P being null
+    input_df = input_df[input_df['Y/N/P'].notnull()]
+    print(f'after removing all rows with Y/N/P targe column being null: {input_df.shape}')
     print(input_df.isnull().sum()/input_df.shape[0])
     input_df.to_csv(output_data, index=False)
-    basename = os.path.splitext(output_data)[0]
-    input_df_not_null = input_df[input_df['Y/N/P'].notnull()]
-    input_df_null = input_df[input_df['Y/N/P'].isna()]
-    input_df_not_null.to_csv(f'{basename}_not_null.csv', index=False)
-    input_df_null.to_csv(f'{basename}_null.csv', index=False)
